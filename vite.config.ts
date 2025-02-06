46e2cf66e7ffe87240e85a915387222de94f79df
robin <nowrobin3@gmail.com>
2025-02-06 11:16:30 +0900

    fix:baseUrl Error:

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/api',
+  base: '/',
   server: {
     proxy: {
       '/api': {
