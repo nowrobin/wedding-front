74c8fbd6cccd526996af5f848bca330946685f56
robin <nowrobin3@gmail.com>
2025-02-07 17:21:48 +0900

    fix: Config env error

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,4 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: '/',
-  define: {
-    'process.env': process.env,
-    VITE_API_URL: process.env.VITE_API_URL,
-    VITE_JAVASCRIPT_KEY: process.env.VITE_JAVASCRIPT_KEY,
-    VITE_KAKAO_KEY: process.env.VITE_KAKAO_KEY,
-    VITE_K_REST_API_KEY: process.env.VITE_K_REST_API_KEY,
-    VITE_NAVER_CLIENT_ID: process.env.VITE_NAVER_CLIENT_ID,
-  },
 });
