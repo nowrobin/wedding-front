1c5e95ddb8c50c312390db05d5fd2350b281f16f
robin <nowrobin3@gmail.com>
2025-02-07 16:42:59 +0900

    fix: typo in config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     'process.env': process.env,
     VITE_API_URL: process.env.API_URL,
     VITE_JAVASCRIPT_KEY: process.env.JAVASCRIPT_KEY,
-    VITE_KAKAO_KEY: process.env.AVASCRIPT_KEY,
+    VITE_KAKAO_KEY: process.env.KAKAO_KEY,
     VITE_K_REST_API_KEY: process.env.K_REST_API_KEY,
     VITE_NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
   },
